2e3045b761198c7d7d5a76c3816f947a25aa3ddb
Artem Pakhomov <setnemo@gmail.com>
2021-02-26 14:58:30 +0200

    update queue

--- queue.php
+++ queue.php
@@ -21,17 +21,12 @@ $database = Database::getInstance()->init($config)->getConnection();
 $trainingRepository = new TrainingRepository($database);
 $exportRepository = new ExportRepository($database);
 $service = new ExportService($trainingRepository, $exportRepository);
-$expectedTime = time() + 60; // +1 min in seconds
-$oneSecond = time();
 while (true) {
-    $now = time();
-    if ($now >= $oneSecond) {
-        $oneSecond = $now + 1;
-        try {
-            $bot->queue($exportRepository, $service);
-            $metric->increaseMetric('queue');
-        } catch (\Throwable $e) {
-            $logger->error($e->getMessage(), $e->getTrace());
-        }
+    try {
+        $bot->queue($exportRepository, $service);
+        $metric->increaseMetric('queue');
+        sleep(4);
+    } catch (\Throwable $e) {
+        $logger->error($e->getMessage(), $e->getTrace());
     }
 }
