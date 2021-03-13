8ec700db1e9ba89cb51dcff34d5321df7b05e6a9
Artem Pakhomov <setnemo@gmail.com>
2021-03-14 01:42:31 +0200

    sonarcloud

--- tests/unit/Bot/Service/CommandService/AlarmServiceTest.php
+++ tests/unit/Bot/Service/CommandService/AlarmServiceTest.php
@@ -4,16 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Bot\Service\CommandService;
 
-use Carbon\Carbon;
 use Codeception\Test\Unit;
-use RepeatBot\Bot\BotHelper;
 use RepeatBot\Bot\Service\CommandService;
 use RepeatBot\Bot\Service\CommandService\CommandOptions;
 use RepeatBot\Bot\Service\CommandService\Commands\AlarmService;
 use RepeatBot\Bot\Service\CommandService\ResponseDirector;
-use RepeatBot\Bot\Service\CommandService\Validators\AlarmValidator;
 use RepeatBot\Bot\Service\CommandService\Validators\Messages\AlarmMessage;
-use RepeatBot\Core\ORM\Entities\Version;
 use UnitTester;
 
 class AlarmServiceTest extends Unit
@@ -41,6 +37,34 @@ class AlarmServiceTest extends Unit
         $this->assertEquals([
             'chat_id' => 42,
             'text' => AlarmMessage::ERROR_TEXT,
+            'parse_mode' => 'markdown',
+            'disable_web_page_preview' => true,
+            'disable_notification' => 1,
+        ], $error->getData());
+    }
+
+    public function testAlarmListEmpty()
+    {
+        $command = new CommandService(
+            options: new CommandOptions(
+                command: 'alarm',
+                payload: explode(' ', 'list'),
+                chatId: 42,
+            )
+        );
+
+        $service = $command->makeService();
+        $this->assertInstanceOf(AlarmService::class, $service);
+    
+        $service->execute();
+        $response = $service->showResponses();
+        /** @var ResponseDirector $error */
+        $error = $response[0];
+        $this->assertInstanceOf(ResponseDirector::class, $error);
+        $this->assertEquals('sendMessage', $error->getType());
+        $this->assertEquals([
+            'chat_id' => 42,
+            'text' => 'Список персональных напоминаний пуст',
             'parse_mode' => 'markdown',
             'disable_web_page_preview' => true,
             'disable_notification' => 1,
